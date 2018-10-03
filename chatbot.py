7f3a44d1953f4ae61186860ebf312e752ba45aaf
Rakshith Vasudev <rakshithvbharani@gmail.com>
2018-10-02 22:06:09 -0700

    add files

--- chatbot.py
+++ chatbot.py
@@ -18,7 +18,28 @@ def read_lines(source):
     return source_lines
 
 
-# get the dataset
+# get the dataset - source file:
+# https://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html
+
+# lines contains the the actual text of each utterance-
+# 	- fields:
+# 		- lineID
+# 		- characterID (who uttered this phrase)
+# 		- movieID
+# 		- character name
+# 		- text of the utterance
+
 lines = read_lines("dataset/movie_lines.txt")
+
+# conversations contains  movie_conversations.txt
+# 	- the structure of the conversations
+# 	- fields
+# 		- characterID of the first character involved in the conversation
+# 		- characterID of the second character involved in the conversation
+# 		- movieID of the movie in which the conversation occurred
+# 		- list of the utterances that make the conversation, in chronological
+# 			order: ['lineID1','lineID2',�,'lineIDN']
+# 			has to be matched with movie_lines.txt to reconstruct the actual content
 conversations = read_lines("dataset/movie_conversations.txt")
+
 
